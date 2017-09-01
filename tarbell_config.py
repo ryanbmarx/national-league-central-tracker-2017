d595ee37e819af462d8fb1867c635e41c518d818
ryanbmarx <ryanbmarx@gmail.com>
2017-09-01 15:11:41 -0500

    honestly I'm just copying a lot of what other folks did

--- tarbell_config.py
+++ tarbell_config.py
@@ -3,6 +3,127 @@
 """
 Tarbell project configuration
 """
+from flask import Blueprint, g, render_template
+import ftfy
+import jinja2
+import xlrd
+from markupsafe import Markup
+
+
+blueprint = Blueprint('nl-central-tracker-2017', __name__)
+
+@blueprint.app_template_filter('jsonify')
+def jsonify_filter(data):
+    return Markup(json.dumps(data))
+
+
+
+@blueprint.app_template_filter('format_date_time')
+def format_date_time(date_time, format_string):
+	# Convert game date from Excel-style date (it's a float) to a
+	# datetime object.
+	#
+	# Assume datetime is 1900-based.
+	# See http://www.lexicon.net/sjmachin/xlrd.html#xlrd.xldate_as_tuple-function
+	datemode = 0
+	date = xlrd.xldate.xldate_as_datetime(date_time, datemode)
+	date = str(date) # Hacky way of dealing with datetime no JSON serializable
+	return date
+
+
+@blueprint.app_template_filter('team_lookup')
+def team_lookup(team_code, requested_format):
+	team_identifiers = {
+		"cubs":{
+			"abbrev": "CHC",
+			"readable": "Chicago Cubs",
+			"readable_short": "Cubs"
+		},
+		"cardinals":{
+			"abbrev": "STL",
+			"readable": "St. Louis Cardinals",
+			"readable_short": "Cardinals"
+		},
+		"pirates":{
+			"abbrev": "PIT",
+			"readable": "Pittsburgh Pirates",
+			"readable_short": "Pirates"
+		},
+		"brewers":{
+			"abbrev": "MIL",
+			"readable": "Milwaukee Brewers",
+			"readable_short": "Brewers"
+		},
+		"astros":{
+			"abbrev": "HOU",
+			"readable": "Houston Astros",
+			"readable_short": "Astros"
+		},
+	}
+
+	# print team_code
+	# print team_lookup
+	# pass
+	return team_identifiers[team_code][requested_format]
+
+@blueprint.app_template_global('merge_data')
+def merge_data(**sheets):
+    data = []
+
+    for name,sheet in sheets.items():
+        team = {}
+
+        team['team_name'] = name
+        team['full_name'] = team_lookup(name, 'readable')
+        team['history'] = team_history(sheet)
+        team['current_record'] = team['history'][len(team['history']) - 1]['record']
+        team['current_games_back'] = team['history'][len(team['history']) - 1]['games_back']
+        team['current_division_rank'] = team['history'][len(team['history']) - 1]['division_rank']
+
+        data.append(team)
+
+    return data
+
+
+def team_history(games):
+    history = []
+
+    for game in games:
+        event = {}
+
+        try:
+            event['result'] = game['WON']
+        except KeyError:
+            continue
+
+        event['game_number'] = game['GAME']
+        event['opponent'] = game['OPPONENT']
+        event['runs_scored'] = game['RUNS']
+        event['runs_allowed'] = game['RUNS_ALLOWED']
+        event['division_rank'] = game['RANK']
+        event['games_above_below_500'] = game['ABOVE_500']
+        event['games_back'] = game['GB'] if game['GB'] > 0 else "-"
+        event['location'] = home_or_away(game)
+        event['record'] = get_current_record(game)
+        # event['game_date'] = excel_date(game['DATE'])
+
+        history.append(event)
+
+    return history
+
+def home_or_away(game):
+    try:
+        game['ROAD_GAME']
+        return 'away'
+    except KeyError:
+        return 'home'
+
+def get_current_record(game):
+	return {
+		"wins": game['TOTAL_WINS'],
+		"losses": game['TOTAL_LOSSES']
+	}
+
 
 # Google spreadsheet key
 SPREADSHEET_KEY = "1P6x6kOSlFiuoRKDJTLOHnCLj8LKczgiI5-WbVzuvr8g"
